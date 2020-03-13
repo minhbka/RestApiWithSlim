2a7e22f1f517546cce3a5b4a6e0c1080c3d8cafb
Phan Minh <minhpv@kormedi.com>
2020-03-13 11:56:04 +0900

    Database Connection

--- public/index.php
+++ public/index.php
@@ -4,13 +4,19 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-
+require __DIR__ . '/../includes/DbConnect.php';
 
 $app = AppFactory::create();
 $app->setBasePath("/MyApi/public");
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+	$response->getBody()->write("Hello, $name");
+	$db = new DbConnect;
+
+	if($db->connect() != null){
+		echo 'Connection Successful'.'<br>';
+	}
+
     return $response;
 });
 
